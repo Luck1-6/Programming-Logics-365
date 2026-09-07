189a73dcbece8f4d05848e66a52adc746363d8c5
Luck1-6 <luckyborn1632004@gmail.com>
2026-09-07 11:23:08 +0530

    day-002: add-two-numbers

---
+++ Phase-1-Foundations/Day002_AddTwoNumbers.py
@@ -0,0 +1,16 @@
+"""
+Headline: Add Two Numbers
+Description: Read two integers and print their sum.
+
+Pseudocode:
+1. input1=int(a)
+2. input2=int(b)
+3. sum = input1 + input2
+4. Print sum
+"""
+
+# ---- Program ----
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+total = a + b
+print("Sum:", total)
