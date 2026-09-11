e91c9cd1f520242b575cb64908e907a194cff3dc
Luck1-6 <luckyborn1632004@gmail.com>
2026-09-11 20:28:02 +0530

    Day011_PrintNumbers1toN.py

---
+++ Phase-1-Foundations/Python/Day011_PrintNumbers1ToN.py
@@ -0,0 +1,16 @@
+"""
+Headline: Print Numbers 1 to N
+Description: Read N and print integers from 1 through N.
+
+Pseudocode:
+n = int(input())
+
+for i in range(1, n + 1):
+  print(i)
+"""
+
+# ---- Program ----
+N = int(input("Enter N: "))
+
+for i in range(1, N + 1):
+    print(i)
