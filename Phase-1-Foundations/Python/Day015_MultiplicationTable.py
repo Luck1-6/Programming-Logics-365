a877ce32ef11494fcc293beccfcb80f2b20656fe
Mayank Singh <luckyborn1632004@gmail.com>
2026-09-15 20:50:48 +0530

    Rename variable 'result' to 'product' in multiplication table

--- Phase-1-Foundations/Python/Day015_MultiplicationTable.py
+++ Phase-1-Foundations/Python/Day015_MultiplicationTable.py
@@ -6,13 +6,13 @@ Pseudocode:
 n = int(input())
 
 for i in range(1, 11):
-  result = n * i
-  print(n, "x", i, "=", result)
+  product = n * i
+  print(n, "x", i, "=", product)
 """
 
 # ---- Program ----
 num = int(input("Enter the number: "))
 
 for i in range(1, 11):
-    result = num * i
-    print(num, "x", i, "=", result)
+    product = num * i
+    print(num, "x", i, "=", product)
