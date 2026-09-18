14e78513bfb13a119b096fe954e2f8a90e3b1d03
Mayank Singh <luckyborn1632004@gmail.com>
2026-09-18 21:06:29 +0530

    Separating functions in Day020_Loop recap

--- Phase-1-Foundations/Java/Day020_LoopsRecap.java
+++ Phase-1-Foundations/Java/Day020_LoopsRecap.java
@@ -1,3 +1,4 @@
+```java
 package Phase_1_foundations;
 
 import java.util.Scanner;
@@ -8,80 +9,111 @@ import java.util.Scanner;
  * reverse, and whether it is a palindrome.
  *
  * Pseudocode:
+ *
  *     input(num);
  *
- *     original = num;
- *     num = Math.abs(num);
+ *     digitCount = countDigits(num);
+ *     digitSum = sumOfDigits(num);
+ *     reverse = reverseNumber(num);
+ *     palindrome = isPalindrome(num);
  *
- *     digitCount = 0;
- *     digitSum = 0;
- *     reverse = 0;
- *
- *     if (num == 0):
- *         digitCount = 1;
- *
- *     else:
- *         while (num != 0):
- *             digit = num % 10;
- *             digitCount++;
- *             digitSum = digitSum + digit;
- *             reverse = reverse * 10 + digit;
- *             num = num / 10;
- *
- *     print("Number of digits: " + digitCount);
- *     print("Sum of digits: " + digitSum);
- *     print("Reversed number: " + reverse);
- *
- *     if (original >= 0 && original == reverse):
- *         print("Palindrome: Yes");
- *     else:
- *         print("Palindrome: No");
-*/
+ *     print all results;
+ */
 
-// ---- Program ----
+// ---- Functions ----
 
 public class Day020_LoopsRecap {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+    // Function 1: Count the number of digits
+    public static int countDigits(int num) {
+
+        num = Math.abs(num);
+
+        if (num == 0) {
+            return 1;
+        }
+
+        int digitCount = 0;
+
+        while (num != 0) {
+            digitCount++;
+            num = num / 10;
+        }
+
+        return digitCount;
+    }
+
+    // Function 2: Calculate the sum of digits
+    public static int sumOfDigits(int num) {
+
+        num = Math.abs(num);
+
+        int digitSum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            digitSum = digitSum + digit;
+            num = num / 10;
+        }
+
+        return digitSum;
+    }
+
+    // Function 3: Reverse the number
+    public static int reverseNumber(int num) {
+
+        num = Math.abs(num);
+
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        return reverse;
+    }
+
+    // Function 4: Check whether the number is palindrome
+    public static boolean isPalindrome(int num) {
+
+        // Negative numbers are not considered palindrome
+        if (num < 0) {
+            return false;
+        }
+
+        int reverse = reverseNumber(num);
+
+        return num == reverse;
+    }
+
+    // ---- Main Program ----
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
-        int original = num;
-        num = Math.abs(num);
-
-        int digitCount = 0;
-        int digitSum = 0;
-        int reverse = 0;
-
-        if (num == 0) {
-            digitCount = 1;
-        } else {
-            while (num != 0) {
-                int digit = num % 10;
-
-                digitCount++;
-                digitSum = digitSum + digit;
-                reverse = reverse * 10 + digit;
-
-                num = num / 10;
-            }
-        }
+        int digitCount = countDigits(num);
+        int digitSum = sumOfDigits(num);
+        int reverse = reverseNumber(num);
+        boolean palindrome = isPalindrome(num);
 
         System.out.println("Number of digits: " + digitCount);
         System.out.println("Sum of digits: " + digitSum);
         System.out.println("Reversed number: " + reverse);
 
-        if (original >= 0 && original == reverse) {
+        if (palindrome) {
             System.out.println("Palindrome: Yes");
         } else {
             System.out.println("Palindrome: No");
         }
 
         sc.close();
-
-	}
-
+    }
 }
+```
+
